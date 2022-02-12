5b268b4fabac28a6ff9032b6004883466702fe3a
Lemos <lemmos@outlook.com.br>
2022-02-12 19:28:26 -0300

    init application

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Início | ig.news</title>
+            </Head>
+
+            <div>
+                <h1>hello world</h1>
+            </div>
+        </>
+    );
+}
