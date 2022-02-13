9c344fe43f90618cf05ce7e262656533df2b0813
Lemos <lemmos@outlook.com.br>
2022-02-12 21:07:28 -0300

    static home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,31 @@
 import Head from 'next/head';
 
+import styles from '../styles/home.module.scss';
+
+import { SubscribeButton } from '../components/SubscribeButton';
+
 export default function Home() {
     return (
         <>
             <Head>
-                <title>Início | ig.news</title>
+                <title>Home | ig.news</title>
             </Head>
 
-            <div>
-                <h1>hello world</h1>
-            </div>
+            <main className={styles.contentContainer}>
+                <section className={styles.hero} >
+                    <span>👋 Hey, welcome</span>
+                    <h1>News about <br /> the <span>React</span> world.</h1>
+
+                    <p>
+                        Get acess to all the publications <br />
+                        <span>for $9.90 monthly</span>
+                    </p>
+
+                    <SubscribeButton />
+                </section>
+
+                <img src="/images/avatar.svg" alt="girl coding" />
+            </main>
         </>
     );
 }
