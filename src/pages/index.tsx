a8fbf3e68bd8d93f9b40127849babccd255c3f05
oLemos <lemmos@outlook.com.br>
2022-04-05 14:57:27 -0300

    stripe integration and server side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
 
 import styles from '../styles/home.module.scss';
 
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps {
+    product: {
+        priceId: string;
+        amount: number;
+    }
+}
+
+export default function Home({ product }: HomeProps) {
     return (
         <>
             <Head>
@@ -18,14 +27,39 @@ export default function Home() {
 
                     <p>
                         Get acess to all the publications <br />
-                        <span>for $9.90 monthly</span>
+                        <span>for {product.amount} monthly</span>
                     </p>
 
-                    <SubscribeButton />
+                    <SubscribeButton priceId={product.priceId} />
                 </section>
 
                 <img src="/images/avatar.svg" alt="girl coding" />
             </main>
         </>
     );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    // O método "retrieve" busca apenas um, e recebe o id do preço.
+    const price = await stripe.prices.retrieve('price_1KSVmVGJ0Ugaf2OBb5KAO4W7')
+
+    // Esta opção "expand" retorna todas as informações do produto.
+    /* const price = await stripe.prices.retrieve('price_1KSVmVGJ0Ugaf2OBb5KAO4W7', {
+        expand: ['product']
+    }) */
+
+    const product = {
+        priceId: price.id,
+        amount: new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+        }).format(price.unit_amount / 100),
+    }
+
+    return {
+        props: {
+            product
+        },
+        revalidate: 60 * 60 * 24, // 24 hours
+    }
 }
