acfccd9cd0f88bfd5b226a673594f29d47ffb081
oLemos <lemmos@outlook.com.br>
2022-04-09 13:59:53 -0300

    first database query

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { query } from 'faunadb';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
     providers: [
@@ -13,4 +16,37 @@ export default NextAuth({
             },
         }),
     ],
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            const { email } = user;
+
+            try {
+                await fauna.query(
+                    query.If(
+                        query.Not(
+                            query.Exists(
+                                query.Match(
+                                    query.Index('user_by_email'),
+                                    query.Casefold(email)
+                                )
+                            )
+                        ),
+                        query.Create(query.Collection('users'), {
+                            data: { email: email },
+                        }),
+                        query.Get(
+                            query.Match(
+                                query.Index('user_by_email'),
+                                query.Casefold(email)
+                            )
+                        )
+                    )
+                );
+
+                return true;
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+    },
 });
